b737993fa31295d776e91c036fb253ce0c53ef4a
GGuppyGit <kylanplunk@gmail.com>
2024-04-08 22:18:35 -0500

    home menu

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -1,25 +1,27 @@
 <script>
-    import Paper, {Content} from '@smui/paper'
-    import Button, { Label } from '@smui/button';
-    import { currentComponent } from "../stores";
+	import Paper, { Content } from '@smui/paper';
+	import Button, { Label } from '@smui/button';
+	import { currentComponent } from '../stores';
+	import { supabase } from '../db';
 </script>
-<Paper class="buttons-paper">
-    <Content class="buttons-content">
-        <Button on:click={() => $currentComponent = 'New'} variant="raised">
-            <Label>New Character</Label>
-        </Button>
-        <Button>
-            <Label>Library</Label>
-        </Button>
-    </Content>
-</Paper> 
-<style>
-:global(.buttons-paper) {
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    padding: 10px;
-}
-</style>
 
+<Paper class="buttons-paper">
+	<Content class="buttons-content">
+		<Button on:click={() => ($currentComponent = 'New')} variant="raised">
+			<Label>New Character</Label>
+		</Button>
+		<Button on:click={() => ($currentComponent = 'Character')}>
+			<Label>Library</Label>
+		</Button>
+	</Content>
+</Paper>
+
+<style>
+	:global(.buttons-paper) {
+		position: absolute;
+		left: 50%;
+		top: 50%;
+		transform: translate(-50%, -50%);
+		padding: 10px;
+	}
+</style>
