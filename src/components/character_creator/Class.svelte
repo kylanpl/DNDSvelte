e6853d9e38f1779fa9f51bb54c24d354a2b2fd57
GGuppyGit <kylanplunk@gmail.com>
2024-04-08 22:17:26 -0500

    class (dnd) component

--- src/components/character_creator/Class.svelte
+++ src/components/character_creator/Class.svelte
@@ -1,9 +1,31 @@
 <script>
+	import Autocomplete from '@smui-extra/autocomplete';
+	import { supabase } from '../../db';
+	let classes = getFromDatabase();
 
-	import Textfield from "@smui/textfield";
-
+	async function getFromDatabase() {
+		let query = supabase.from('classes').select('name');
+		const { data, error } = await query;
+		if (error) console.log(error);
+		console.log(data);
+		/**
+		 * @type {string[]}
+		 */
+		let classes = [];
+		data?.forEach((item) => {
+			classes.push(item.name.charAt(0).toUpperCase() + item.name.slice(1));
+		});
+		return classes;
+	}
 </script>
-<h2>Class</h2>
-<Textfield value=''
-label="Class">
-</Textfield>
+
+<div>
+	<h2>Class</h2>
+	<Autocomplete options={classes} label="Class" />
+</div>
+
+<style>
+	div {
+		margin: 3px;
+	}
+</style>
