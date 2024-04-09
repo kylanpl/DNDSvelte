405d5ea4e66027b795699ee609b178d16af564ae
GGuppyGit <kylanplunk@gmail.com>
2024-04-08 22:16:15 -0500

    proficiency component, part of proficiencies

---
+++ src/components/character_creator/Proficiencies.svelte
@@ -0,0 +1,45 @@
+<script>
+    import Proficiency from "./Proficiency.svelte";
+    import LayoutGrid, { Cell } from '@smui/layout-grid';
+
+    let proficiencyArray = [
+        "Acrobatics",
+        "Animal Handling",
+        "Arcana",
+        "Athletics",
+        "Deception",
+        "History",
+        "Insight",
+        "Intimidation",
+        "Investigation",
+        "Medicine",
+        "Nature",
+        "Perception",
+        "Performance",
+        "Persuasion",
+        "Religion",
+        "Sleight of Hand",
+        "Stealth",
+        "Survival"
+    ];
+</script>
+
+<div>
+    <h2>Proficiencies</h2>
+
+    <LayoutGrid class="profgrid">
+        {#each proficiencyArray as proficiency, i}
+            <Cell class="profgrid" span=5>
+                <Proficiency type={proficiency} />
+            </Cell>
+        {/each}
+      </LayoutGrid>
+
+    
+</div>
+
+<style>
+.profgrid {
+    padding: 0;
+}
+</style>
---
+++ src/components/character_creator/Proficiency.svelte
@@ -0,0 +1,21 @@
+
+<div>
+    <FormField>
+    <Checkbox bind:checked />
+    <span slot="label">{type}</span>
+  </FormField>
+</div>
+
+<style>
+</style>
+
+<script>
+    import Checkbox from '@smui/checkbox';
+    import FormField from '@smui/form-field';
+  
+    let checked = false;
+    /**
+   * @type {string}
+   */
+    export let type;
+</script>
