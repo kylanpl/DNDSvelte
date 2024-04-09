f6e6a6091ecd6ccba38bf5afa1b5373d1c7f123b
GGuppyGit <kylanplunk@gmail.com>
2024-04-08 22:14:21 -0500

    add capability to load character component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 
     import Home from '../components/Home.svelte'
     import New from '../components/New.svelte'
+    import Character from '../components/Character.svelte'
     /**
 	 * @type {typeof Home}
 	 */
@@ -14,6 +15,9 @@
       }
         else if ($currentComponent === 'New') {
         CurrentComp = New;
+        }
+        else if ($currentComponent === 'Character') {
+        CurrentComp = Character;
         }
     }
     
