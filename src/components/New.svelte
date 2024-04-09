a671ff191c3ac999e87da18634231d5a0d145947
GGuppyGit <kylanplunk@gmail.com>
2024-04-08 22:18:06 -0500

    character creator

--- src/components/New.svelte
+++ src/components/New.svelte
@@ -1,66 +1,80 @@
 <script>
-// @ts-nocheck
+	// @ts-nocheck
 
-	import Paper, { Content } from "@smui/paper";
-	import { Section } from "@smui/top-app-bar";
-    import Textfield from '@smui/textfield';
-    import HelperText from '@smui/textfield/helper-text';
-	import { Label } from "@smui/common";
-    import Card, { Actions } from '@smui/card';
-    import Demographic from "./character_creator/Demographic.svelte"
-	import Button from "@smui/button";
-    import Class from "./character_creator/Class.svelte";
+	import Paper, { Content } from '@smui/paper';
+	import { Section } from '@smui/top-app-bar';
+	import Textfield from '@smui/textfield';
+	import HelperText from '@smui/textfield/helper-text';
+	import { Label } from '@smui/common';
+	import Card, { Actions } from '@smui/card';
+	import Demographic from './character_creator/Demographic.svelte';
+	import Button from '@smui/button';
+	import Class from './character_creator/Class.svelte';
 
+	import Functional from './character_creator/Functional.svelte';
+	import AbilityScores from './character_creator/AbilityScores.svelte';
+	import { currentComponent } from '../stores';
 
-    let page = 0;
+	let page = 0;
 
-    const CharacterCreatorPages = {
-        0 : Demographic,
-        1 : Class,
-    }
-
-
+	const CharacterCreatorPages = {
+		0: Demographic,
+		1: Functional,
+		2: AbilityScores
+	};
 </script>
 
+<div class="creator-window-container">
+	<h1	h1>Character Creator</h1>
+	<div class="creator-window">
+	<Card padded>
+		<svelte:component this={CharacterCreatorPages[page]} />
+		{#if page == 0}
+			<Actions class="next">
+				<Button on:click={() => page++}>
+					<i class="material-icons test" aria-hidden="true">arrow_forward</i>
+				</Button>
+			</Actions>
+		{:else if page < CharacterCreatorPages.length - 1}
+			<Actions class="next-back">
+				<Button on:click={() => page--}>
+					<i class="material-icons" aria-hidden="true">arrow_backward</i>
+				</Button>
+				<Button on:click={() => page++}>
+					<i class="material-icons" aria-hidden="true">arrow_forward</i>
+				</Button>
+			</Actions>
+		{:else}
+			<Actions class="next-back">
+				<Button on:click={() => page--}>
+					<i class="material-icons" aria-hidden="true">arrow_backward</i>
+				</Button>
+				<Button on:click={() => $currentComponent = 'Home'}>
+					<i class="material-icons" aria-hidden="true">done</i>
+				</Button>
+			</Actions>
+		{/if}
 
-<div class="creator-window">
-    <h1>Character Creator</h1>
-        <Card padded >
-
-            <svelte:component this={CharacterCreatorPages[page]} />
-            {#if page == 0}
-            <Actions class="next">
-                <Button on:click={ () => page++}>
-                    <i class="material-icons test" aria-hidden="true">arrow_forward</i>
-                </Button>
-            </Actions>
-            {:else}
-            <Actions class="next-back">
-                <Button on:click={ () => page--}>
-                    <i class="material-icons" aria-hidden="true">arrow_backward</i>
-                </Button>
-                <Button on:click={ () => page++}>
-                    <i class="material-icons" aria-hidden="true">arrow_forward</i>
-                </Button>
-            </Actions>
-            {/if}
-
-        </Card>
+	</Card>
+	</div>
 </div>
 
 <style>
-    :global(.creator-window) {
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    }
 
-    :global(.next) {
-    justify-content: right;
-    }
+	:global(.creator-window-container) {
+		display: flex;
+		flex-direction: column;
+	}
+	:global(.creator-window) {
+		display: flex;
+        margin: auto;
+	}
 
-    :global(.next-back) {
-    justify-content: space-between;
-    }
+	:global(.next) {
+		justify-content: right;
+	}
+
+	:global(.next-back) {
+		justify-content: space-between;
+	}
 </style>
