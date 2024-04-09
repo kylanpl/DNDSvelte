3b3a3b72a4d8caedbab63d2095e947ca687d22f7
GGuppyGit <kylanplunk@gmail.com>
2024-04-08 22:15:55 -0500

    stat componment, part of ability scores

---
+++ src/components/character_creator/Stat.svelte
@@ -0,0 +1,30 @@
+<script>
+	import Textfield from '@smui/textfield';
+	import HelperText from '@smui/textfield/helper-text';
+	/**
+	 * @type {string}
+	 */
+	export let statString = '';
+	/**
+	 * @type {number}
+	 */
+	export let value;
+
+	export function randChar() {}
+</script>
+
+<div>
+	<div>
+		<p>
+			<Textfield style="width: 50px" bind:value label="" type="number">
+				<HelperText persistent slot="helper">{statString.toUpperCase()}</HelperText>
+			</Textfield>
+		</p>
+	</div>
+</div>
+
+<style>
+	* {
+		margin: 0;
+	}
+</style>
