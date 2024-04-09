3895104733d988b13101ee41da7792c066b1eb92
GGuppyGit <kylanplunk@gmail.com>
2024-04-08 22:16:37 -0500

    functional fields of character creator

---
+++ src/components/character_creator/Functional.svelte
@@ -0,0 +1,37 @@
+<script>
+	import AbilityScores from './AbilityScores.svelte';
+	import Race from './Race.svelte';
+	import Class from './Class.svelte';
+	import Background from './Background.svelte';
+	import Proficiencies from './Proficiencies.svelte';
+</script>
+
+<div>
+	<div class="row">
+		<div class="column">
+			<div class="row">
+				<Class />
+				<Race />
+                <Background />
+			</div>
+			<div class="row ability-scores">
+				<AbilityScores />
+                <Proficiencies />
+			</div>
+            <div>
+                
+            </div>
+        </div>
+	</div>
+</div>
+
+<style>
+	.row {
+		display: flex;
+	}
+
+	.column {
+		display: flex;
+		flex-direction: column;
+	}
+</style>
